c4df6ed16d57c7f1222fc678aa51def1a5aa8a4e
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2025-08-27 20:02:39 +0300

    feat(header): add links to each page in the app

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,8 +1,15 @@
+const { NavLink } = ReactRouterDOM;
+
 export default function AppHeader() {
   return (
     <header className="app-header full main-layout">
       <section className="header-container">
         <h1>React Starter Proj</h1>
+        <nav className="header-nav">
+          <NavLink to="/home">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/books">Books</NavLink>
+        </nav>
       </section>
     </header>
   );
