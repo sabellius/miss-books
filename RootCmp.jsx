ae3fd5127490510fd28d2c5083d1e74e4771b72b
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2025-08-27 17:49:28 +0300

    feat: create components with placeholders for the app pages

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,13 +1,28 @@
-import { AppHeader } from "./cmps/AppHeader.jsx";
-import { Home } from "./cmps/Home.jsx";
+import AppHeader from "./cmps/AppHeader.jsx";
+import About from "./pages/About.jsx";
+import BookDetails from "./pages/BookDetails.jsx";
+import BookIndex from "./pages/BookIndex.jsx";
+import Home from "./pages/Home.jsx";
+
+const Router = ReactRouterDOM.HashRouter;
+const { Routes, Route, Navigate } = ReactRouterDOM;
 
 export function RootCmp() {
   return (
-    <section className="app main-layout">
-      <AppHeader />
-      <main>
-        <Home />
-      </main>
-    </section>
+    <Router>
+      <section className="app main-layout">
+        <AppHeader />
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/books" element={<BookIndex />} />
+            <Route path="/books/:bookId" element={<BookDetails />} />
+          </Routes>
+        </main>
+      </section>
+    </Router>
   );
 }
