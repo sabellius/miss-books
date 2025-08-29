1846b77266cb412f58c413383ffb225b17993a48
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2025-08-29 16:25:49 +0300

    fix: add services to source control

--- services/util.service.js
+++ services/util.service.js
@@ -10,9 +10,9 @@ export const utilService = {
 };
 
 function makeId(length = 6) {
-  var txt = "";
+  var txt = '';
   var possible =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
   for (var i = 0; i < length; i++) {
     txt += possible.charAt(Math.floor(Math.random() * possible.length));
@@ -23,40 +23,40 @@ function makeId(length = 6) {
 
 function makeLorem(size = 100) {
   const words = [
-    "The sky",
-    "above",
-    "the port",
-    "was",
-    "the color",
-    "of nature",
-    "tuned",
-    "to",
-    "a live channel",
-    "All",
-    "this happened",
-    "more or less",
-    "I",
-    "had",
-    "the story",
-    "bit by bit",
-    "from various people",
-    "and",
-    "as generally",
-    "happens",
-    "in such cases",
-    "each time",
-    "it",
-    "was",
-    "a different story",
-    "a pleasure",
-    "to",
-    "burn",
+    'The sky',
+    'above',
+    'the port',
+    'was',
+    'the color',
+    'of nature',
+    'tuned',
+    'to',
+    'a live channel',
+    'All',
+    'this happened',
+    'more or less',
+    'I',
+    'had',
+    'the story',
+    'bit by bit',
+    'from various people',
+    'and',
+    'as generally',
+    'happens',
+    'in such cases',
+    'each time',
+    'it',
+    'was',
+    'a different story',
+    'a pleasure',
+    'to',
+    'burn',
   ];
-  var txt = "";
+  var txt = '';
   while (size > 0) {
     size--;
     txt += words[Math.floor(Math.random() * words.length)];
-    if (size >= 1) txt += " ";
+    if (size >= 1) txt += ' ';
   }
   return txt;
 }
@@ -78,38 +78,38 @@ function loadFromStorage(key) {
 
 function getDayName(date, locale) {
   date = new Date(date);
-  return date.toLocaleDateString(locale, { weekday: "long" });
+  return date.toLocaleDateString(locale, { weekday: 'long' });
 }
 
 function getMonthName(date) {
   const monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
   ];
   return monthNames[date.getMonth()];
 }
 
-function animateCSS(el, animation = "bounce") {
-  const prefix = "animate__";
+function animateCSS(el, animation = 'bounce') {
+  const prefix = 'animate__';
   return new Promise((resolve, reject) => {
     const animationName = `${prefix}${animation}`;
     el.classList.add(`${prefix}animated`, animationName);
     function handleAnimationEnd(event) {
       event.stopPropagation();
       el.classList.remove(`${prefix}animated`, animationName);
-      resolve("Animation ended");
+      resolve('Animation ended');
     }
 
-    el.addEventListener("animationend", handleAnimationEnd, { once: true });
+    el.addEventListener('animationend', handleAnimationEnd, { once: true });
   });
 }
