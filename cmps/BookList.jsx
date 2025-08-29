a57ef6e985338419a1351eaee4bd65834300eb22
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2025-08-29 17:41:38 +0300

    feat: add a responsive design to the book list

--- cmps/BookList.jsx
+++ cmps/BookList.jsx
@@ -2,16 +2,19 @@ import { utilService } from '../services/util.service.js';
 
 export default function BookList({ books }) {
   return (
-    <section className="book-list container flex justify-center wrap">
+    <section className="book-list flex justify-center">
       {books.map(book => (
         <div key={book.id} className="book-preview">
-          <h2>{book.title}</h2>
-          <h3>{book.subtitle}</h3>
           <img
+            className="book-img"
             src={utilService.fixThumbnailUrl(book.thumbnail)}
             alt={book.title}
             width={'200px'}
           />
+          <div className="book-info">
+            <h2>{book.title}</h2>
+            <h3>{book.subtitle}</h3>
+          </div>
         </div>
       ))}
     </section>
