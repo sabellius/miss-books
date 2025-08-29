cb9120241303e240818e7cb2c7ea896f0445de73
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2025-08-29 19:22:18 +0300

    feat: add a link to the book details of each book

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -1,18 +1,21 @@
 import { utilService } from '../services/util.service.js';
+const { Link } = ReactRouterDOM;
 
 export default function BookPreview({ book }) {
   return (
     <div className="book-preview">
-      <img
-        className="book-img"
-        src={utilService.fixThumbnailUrl(book.thumbnail)}
-        alt={book.title}
-        width={'200px'}
-      />
-      <div className="book-info">
-        <h2>{book.title}</h2>
-        <h3>{book.subtitle}</h3>
-      </div>
+      <Link to={`/books/${book.id}`}>
+        <img
+          className="book-img"
+          src={utilService.fixThumbnailUrl(book.thumbnail)}
+          alt={book.title}
+          width={'200px'}
+        />
+        <div className="book-info">
+          <h2>{book.title}</h2>
+          <h3>{book.subtitle}</h3>
+        </div>
+      </Link>
     </div>
   );
 }
