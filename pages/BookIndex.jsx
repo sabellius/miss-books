7e52b38ff85dca6f0c5c80fbc59e5e9c4b1994d7
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2025-08-29 16:17:06 +0300

    feat: add a BookIndex page

--- pages/BookIndex.jsx
+++ pages/BookIndex.jsx
@@ -1,3 +1,18 @@
+const { useState, useEffect } = React;
+import { query } from '../services/book.service.js';
+import BookList from '../cmps/BookList.jsx';
+
 export default function BookIndex() {
-  return <div>BookIndex</div>;
+  const [books, setBooks] = useState(null);
+
+  useEffect(() => {
+    loadBooks();
+  }, []);
+
+  function loadBooks() {
+    query().then(books => setBooks(books));
+  }
+
+  if (!books) return <div>Loading...</div>;
+  return <BookList books={books} />;
 }
