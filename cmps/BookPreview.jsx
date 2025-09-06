0e125e7a35774e63d53f45d6a97f22758b986646
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2025-09-06 10:55:44 +0300

    feat: add buttons to view/edit/delete to the book preview

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -1,21 +1,30 @@
-import { utilService } from '../services/util.service.js';
 const { Link } = ReactRouterDOM;
+import { remove } from '../services/book.service.js';
 
 export default function BookPreview({ book }) {
   return (
     <div className="book-preview">
-      <Link to={`/books/${book.id}`}>
-        <img
-          className="book-img"
-          src={book.thumbnail}
-          alt={book.title}
-          width={'200px'}
-        />
-        <div className="book-info">
-          <h2>{book.title}</h2>
-          <h3>{book.subtitle}</h3>
-        </div>
-      </Link>
+      <img
+        className="book-img"
+        src={book.thumbnail}
+        alt={book.title}
+        width={'200px'}
+      />
+      <div className="book-info">
+        <h2>{book.title}</h2>
+        <h3>{book.subtitle}</h3>
+      </div>
+      <div className="book-actions flex">
+        <Link to={`/books/${book.id}`}>
+          <button>Details</button>
+        </Link>
+        <Link to={`/books/${book.id}/edit`}>
+          <button>Edit</button>
+        </Link>
+        <span className="delete-action" onClick={() => remove(book.id)}>
+          <button>Delete</button>
+        </span>
+      </div>
     </div>
   );
 }
